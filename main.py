fa60808c1b716702b762ccd46f15d7a718d033f6
sans18-coder <iksanwijaya018@gmail.com>
2024-05-31 20:31:31 +0700

    ku tambahin kodingan dikit sama ku pindahin folder training model ke repo prediction repo ini khusus buat api nya doang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import pandas as pd
 import numpy as np
+import csv
+import random
 from http import HTTPStatus
 import tensorflow as tf
 from flask import Flask, jsonify, request
@@ -11,15 +13,15 @@ load_dotenv()
 
 app = Flask(__name__)
 # Hint for anak CC: mungkin bsa di tambahin codingan buat masukin inputan user sama outputnya ke bucket buat data tambahan training model
-# app.config['UPLOAD_FOLDER'] = 'Users/uploads/'
-app.config['MODEL_CLASSIFICATION'] = './Training-model/model/stunting_prediction.h5'
-# app.config['GCS_CREDENTIALS'] = './credentials/gcs.json'
+app.config['UPLOAD_FOLDER'] = 'Users/uploads/'
+app.config['MODEL_CLASSIFICATION'] = './model/stunting_prediction.h5'
+app.config['GCS_CREDENTIALS'] = './credentials/gcs.json'
 
 model = tf.keras.models.load_model(app.config['MODEL_CLASSIFICATION'],compile=False)
 
-# bucket_name = os.environ.get('BUCKET_NAME','data-balita')
-# client = storage.Client.from_service_account_json(json_credentials_path=app.config['GCS_CREDENTIALS'])
-# bucket = storage.Bucket(client,bucket_name)
+bucket_name = os.environ.get('BUCKET_NAME','data-balita')
+client = storage.Client.from_service_account_json(json_credentials_path=app.config['GCS_CREDENTIALS'])
+bucket = storage.Bucket(client,bucket_name)
 
 classes = ["Stunting Berat","Stunting","Normal","Tinggi"] 
 
@@ -40,7 +42,7 @@ def predict_stunting():
         day = float(request.form["day"])
         jenis_kelamin = request.form["jenis_kelamin"]
         tinggi_badan = float(request.form["tinggi_badan"])
-        if  year and month and day and jenis_kelamin and tinggi_badan:
+        if year is not None and month is not None and day is not None and jenis_kelamin is not None and tinggi_badan is not None:
             umur = ((year*12) + month) + (day/30)
             jenis_kelamin_map = {'laki-laki': 0, 'perempuan': 1}
             jenis_kelamin_num = jenis_kelamin_map[jenis_kelamin]
@@ -52,6 +54,24 @@ def predict_stunting():
                 'class' : classes[np.argmax(prediction_result)],
                 'presentase' : str("{:.1f}".format(np.max(prediction_result)*100))
             }
+            
+            data = {
+                "Umur (bulan)" : umur,
+                "Jenis Kelamin" : jenis_kelamin,
+                "Tinggi Badan (cm)" : tinggi_badan,
+                "Status Gizi" : classes[np.argmax(prediction_result)]
+            }
+            file_csv = "inputan_user.csv"
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'],file_csv)
+            with open(file_path, mode='w', newline='') as file:
+                fieldnames = ["Umur (bulan)", "Jenis Kelamin", "Tinggi Badan (cm)","Status Gizi"]
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
+                for row in data:
+                    writer.writerow(row)
+            blob = bucket.blob('Users_input/'+file_csv+str(random.randint(10000,99999)) )
+            blob.upload_from_filename(file_path)
+            os.remove(file_path)
             return jsonify({
                 "status" : {
                     "code" : HTTPStatus.OK,
