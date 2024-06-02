0adf8fc64d7afc4d6488d4bb2340db3c9f0a1823
sans18-coder <116364950+sans18-coder@users.noreply.github.com>
2024-06-02 21:17:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,7 @@ def predict_stunting():
                 fieldnames = ["Umur (bulan)", "Jenis Kelamin", "Tinggi Badan (cm)","Status Gizi"]
                 writer = csv.DictWriter(file, fieldnames=fieldnames)
                 writer.writeheader()
-                for row in data:
-                    writer.writerow(row)
+                writer.writerow(data)
             blob = bucket.blob('Users_input/'+file_csv+str(random.randint(10000,99999)) )
             blob.upload_from_filename(file_path)
             os.remove(file_path)
